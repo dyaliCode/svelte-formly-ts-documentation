d9249f11944b086ab90fa1090dee54979fee4023
kamalkech <maroc.develop@gmail.com>
2022-07-23 17:15:21 +0100

    update text mdx

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,104 +1,133 @@
-<script>
-	import '../app.css';
-</script>
+<header class="py-10">
+	<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+		<nav class="relative z-50 flex justify-between">
+			<div class="flex items-center md:gap-x-12">
+				<div class="sm:hidden md:flex md:gap-x-6">
+					<a
+						class="inline-block rounded-lg py-1 px-2 text-sm text-slate-700 hover:bg-slate-100 hover:text-slate-900"
+						href="/docs/getting-started">Getting Started</a
+					><a
+						class="inline-block rounded-lg py-1 px-2 text-sm text-slate-700 hover:bg-slate-100 hover:text-slate-900"
+						href="/docs/components">Components</a
+					><a
+						class="inline-block rounded-lg py-1 px-2 text-sm text-slate-700 hover:bg-slate-100 hover:text-slate-900"
+						href="/docs/examples">Examples</a
+					>
+				</div>
+			</div>
 
-<header class="w-full bg-gray-700 text-slate-50 text-center pt-32 pb-32">
-	<h1 class="text-5xl drop-shadow-md">
-		<span class="bg-slate-50 text-orange-500 rounded-full p-4 shadow-lg shadow-slate-500/50"
-			>SF</span
-		>
-		Svelte Formly
-	</h1>
-	<p class="mt-10 mx-auto text-xl w-1/3 text-center tracking-wider drop-shadow-md">
-		A good solution to generate and control a dynamic forms using core and custom rules with
-		customize styles.
-	</p>
-
-	<div class="mt-10 mx-auto sm:w-full lg:w-1/4 flex space-x-4 justify-center">
-		<div class="flex-1">
-			<a
-				class="w-full shadow-lg inline-flex items-center justify-center rounded-full p-4 text-sm font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-slate-900 text-white hover:bg-slate-700 hover:text-slate-100 active:bg-slate-800 active:text-slate-300 focus-visible:outline-slate-900"
-				href="/docs/getting-started/quick-start">Getting Started</a
-			>
-		</div>
-		<div class="flex-1">
-			<a
-				class="w-full shadow-lg inline-flex items-center justify-center rounded-full p-4 text-sm font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-slate-900 text-white hover:bg-slate-700 hover:text-slate-100 active:bg-slate-800 active:text-slate-300 focus-visible:outline-slate-900"
-				href="https://github.com/dyaliCode/svelte-formly"
-				target="_blink">Github</a
-			>
-		</div>
+			<div class="flex items-center gap-x-5 md:gap-x-8">
+				<div class="md:block">
+					<a
+						class="group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-gray-900 text-white hover:text-slate-100 hover:bg-orange-500 active:bg-orange-800 active:text-orange-100 focus-visible:outline-orange-600"
+						href="https://github.com/dyaliCode/svelte-formly"
+						target="_blink"><span>Github</span></a
+					>
+				</div>
+			</div>
+		</nav>
 	</div>
 </header>
 
-<div
-	class="mx-auto mt-10 sm:w-3/4 md:w-3/5 grid gap-4 xl:grid-cols-3 lg:grid-cols-1 sm:grid-cols-1 text-slate-50"
->
-	<div class="bg-orange-500 shadow-lg shadow-rose-600/50 text-center p-10 rounded-3xl">
-		<div class="w-full">
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				width="16"
-				height="16"
-				fill="currentColor"
-				class="bi bi-cpu w-full h-20"
-				viewBox="0 0 16 16"
-			>
-				<path
-					d="M5 0a.5.5 0 0 1 .5.5V2h1V.5a.5.5 0 0 1 1 0V2h1V.5a.5.5 0 0 1 1 0V2h1V.5a.5.5 0 0 1 1 0V2A2.5 2.5 0 0 1 14 4.5h1.5a.5.5 0 0 1 0 1H14v1h1.5a.5.5 0 0 1 0 1H14v1h1.5a.5.5 0 0 1 0 1H14v1h1.5a.5.5 0 0 1 0 1H14a2.5 2.5 0 0 1-2.5 2.5v1.5a.5.5 0 0 1-1 0V14h-1v1.5a.5.5 0 0 1-1 0V14h-1v1.5a.5.5 0 0 1-1 0V14h-1v1.5a.5.5 0 0 1-1 0V14A2.5 2.5 0 0 1 2 11.5H.5a.5.5 0 0 1 0-1H2v-1H.5a.5.5 0 0 1 0-1H2v-1H.5a.5.5 0 0 1 0-1H2v-1H.5a.5.5 0 0 1 0-1H2A2.5 2.5 0 0 1 4.5 2V.5A.5.5 0 0 1 5 0zm-.5 3A1.5 1.5 0 0 0 3 4.5v7A1.5 1.5 0 0 0 4.5 13h7a1.5 1.5 0 0 0 1.5-1.5v-7A1.5 1.5 0 0 0 11.5 3h-7zM5 6.5A1.5 1.5 0 0 1 6.5 5h3A1.5 1.5 0 0 1 11 6.5v3A1.5 1.5 0 0 1 9.5 11h-3A1.5 1.5 0 0 1 5 9.5v-3zM6.5 6a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3z"
-				/>
-			</svg>
-		</div>
-		<h2 class="text-lg font-semibold my-5">Generator</h2>
-		<p>
-			Using object list fields with attributes, you can generate automatically a dynamic form with
-			custom rules.
+<main>
+	<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pt-20 pb-16 text-center lg:pt-32">
+		<h1
+			class="mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl"
+		>
+			<span class="relative whitespace-nowrap text-orange-600"
+				><span class="relative">Svelte</span></span
+			> Formly
+		</h1>
+		<p class="mx-auto mt-6 max-w-2xl text-lg tracking-tight text-slate-700">
+			A good solution to generate and control a dynamic forms using core and custom rules with
+			customize styles.
 		</p>
-	</div>
-	<div class="bg-blue-500 shadow-lg shadow-blue-600/50 text-center p-10 rounded-3xl">
-		<div class="w-full">
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				width="16"
-				height="16"
-				fill="currentColor"
-				class="bi bi-brush w-full h-20"
-				viewBox="0 0 16 16"
+		<div class="mt-10 flex justify-center gap-x-6">
+			<a
+				class="group inline-flex items-center justify-center rounded-full py-4 px-8 text-sm font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-slate-600 text-white hover:bg-slate-700 hover:text-slate-100 active:bg-slate-800 active:text-slate-300 focus-visible:outline-slate-900"
+				href="/docs/getting-started/quick-start">Quick Start</a
 			>
-				<path
-					d="M15.825.12a.5.5 0 0 1 .132.584c-1.53 3.43-4.743 8.17-7.095 10.64a6.067 6.067 0 0 1-2.373 1.534c-.018.227-.06.538-.16.868-.201.659-.667 1.479-1.708 1.74a8.118 8.118 0 0 1-3.078.132 3.659 3.659 0 0 1-.562-.135 1.382 1.382 0 0 1-.466-.247.714.714 0 0 1-.204-.288.622.622 0 0 1 .004-.443c.095-.245.316-.38.461-.452.394-.197.625-.453.867-.826.095-.144.184-.297.287-.472l.117-.198c.151-.255.326-.54.546-.848.528-.739 1.201-.925 1.746-.896.126.007.243.025.348.048.062-.172.142-.38.238-.608.261-.619.658-1.419 1.187-2.069 2.176-2.67 6.18-6.206 9.117-8.104a.5.5 0 0 1 .596.04zM4.705 11.912a1.23 1.23 0 0 0-.419-.1c-.246-.013-.573.05-.879.479-.197.275-.355.532-.5.777l-.105.177c-.106.181-.213.362-.32.528a3.39 3.39 0 0 1-.76.861c.69.112 1.736.111 2.657-.12.559-.139.843-.569.993-1.06a3.122 3.122 0 0 0 .126-.75l-.793-.792zm1.44.026c.12-.04.277-.1.458-.183a5.068 5.068 0 0 0 1.535-1.1c1.9-1.996 4.412-5.57 6.052-8.631-2.59 1.927-5.566 4.66-7.302 6.792-.442.543-.795 1.243-1.042 1.826-.121.288-.214.54-.275.72v.001l.575.575zm-4.973 3.04.007-.005a.031.031 0 0 1-.007.004zm3.582-3.043.002.001h-.002z"
-				/>
-			</svg>
+			<!-- <a
+					class="group inline-flex ring-1 items-center justify-center rounded-full py-2 px-4 text-sm focus:outline-none ring-slate-200 text-slate-700 hover:text-slate-900 hover:ring-slate-300 active:bg-slate-100 active:text-slate-600 focus-visible:outline-blue-600 focus-visible:ring-slate-300"
+					href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+					><svg aria-hidden="true" class="h-3 w-3 flex-none fill-blue-600 group-active:fill-current"
+						><path
+							d="m9.997 6.91-7.583 3.447A1 1 0 0 1 1 9.447V2.553a1 1 0 0 1 1.414-.91L9.997 5.09c.782.355.782 1.465 0 1.82Z"
+						/></svg
+					><span class="ml-3">Watch video</span></a
+				> -->
 		</div>
-		<h2 class="text-lg font-semibold my-5">Generator</h2>
-		<p>
-			Using object list fields with attributes, you can generate automatically a dynamic form with
-			custom rules.
-		</p>
 	</div>
-	<div class="bg-zinc-500 shadow-lg shadow-zinc-600/50 text-center p-10 rounded-3xl">
-		<div class="w-full">
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				width="16"
-				height="16"
-				fill="currentColor"
-				class="bi bi-speedometer2 w-full h-20"
-				viewBox="0 0 16 16"
-			>
-				<path
-					d="M8 4a.5.5 0 0 1 .5.5V6a.5.5 0 0 1-1 0V4.5A.5.5 0 0 1 8 4zM3.732 5.732a.5.5 0 0 1 .707 0l.915.914a.5.5 0 1 1-.708.708l-.914-.915a.5.5 0 0 1 0-.707zM2 10a.5.5 0 0 1 .5-.5h1.586a.5.5 0 0 1 0 1H2.5A.5.5 0 0 1 2 10zm9.5 0a.5.5 0 0 1 .5-.5h1.5a.5.5 0 0 1 0 1H12a.5.5 0 0 1-.5-.5zm.754-4.246a.389.389 0 0 0-.527-.02L7.547 9.31a.91.91 0 1 0 1.302 1.258l3.434-4.297a.389.389 0 0 0-.029-.518z"
-				/>
-				<path
-					fill-rule="evenodd"
-					d="M0 10a8 8 0 1 1 15.547 2.661c-.442 1.253-1.845 1.602-2.932 1.25C11.309 13.488 9.475 13 8 13c-1.474 0-3.31.488-4.615.911-1.087.352-2.49.003-2.932-1.25A7.988 7.988 0 0 1 0 10zm8-7a7 7 0 0 0-6.603 9.329c.203.575.923.876 1.68.63C4.397 12.533 6.358 12 8 12s3.604.532 4.923.96c.757.245 1.477-.056 1.68-.631A7 7 0 0 0 8 3z"
-				/>
-			</svg>
+
+	<div
+		class="mx-auto mt-4 sm:w-3/4 md:w-3/5 grid gap-4 xl:grid-cols-3 lg:grid-cols-1 sm:grid-cols-1 text-slate-50"
+	>
+		<div class="bg-orange-400 shadow-lg shadow-rose-400/50 text-center p-10 rounded-xl">
+			<div class="w-full">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="16"
+					height="16"
+					fill="currentColor"
+					class="bi bi-cpu w-full h-10"
+					viewBox="0 0 16 16"
+				>
+					<path
+						d="M5 0a.5.5 0 0 1 .5.5V2h1V.5a.5.5 0 0 1 1 0V2h1V.5a.5.5 0 0 1 1 0V2h1V.5a.5.5 0 0 1 1 0V2A2.5 2.5 0 0 1 14 4.5h1.5a.5.5 0 0 1 0 1H14v1h1.5a.5.5 0 0 1 0 1H14v1h1.5a.5.5 0 0 1 0 1H14v1h1.5a.5.5 0 0 1 0 1H14a2.5 2.5 0 0 1-2.5 2.5v1.5a.5.5 0 0 1-1 0V14h-1v1.5a.5.5 0 0 1-1 0V14h-1v1.5a.5.5 0 0 1-1 0V14h-1v1.5a.5.5 0 0 1-1 0V14A2.5 2.5 0 0 1 2 11.5H.5a.5.5 0 0 1 0-1H2v-1H.5a.5.5 0 0 1 0-1H2v-1H.5a.5.5 0 0 1 0-1H2v-1H.5a.5.5 0 0 1 0-1H2A2.5 2.5 0 0 1 4.5 2V.5A.5.5 0 0 1 5 0zm-.5 3A1.5 1.5 0 0 0 3 4.5v7A1.5 1.5 0 0 0 4.5 13h7a1.5 1.5 0 0 0 1.5-1.5v-7A1.5 1.5 0 0 0 11.5 3h-7zM5 6.5A1.5 1.5 0 0 1 6.5 5h3A1.5 1.5 0 0 1 11 6.5v3A1.5 1.5 0 0 1 9.5 11h-3A1.5 1.5 0 0 1 5 9.5v-3zM6.5 6a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3z"
+					/>
+				</svg>
+			</div>
+			<h2 class="font-semibold my-5">Generator</h2>
+			<p class="mt-2 font-display text-md text-slate-50">
+				Using object list fields with attributes, you can generate automatically a dynamic form with
+				custom rules.
+			</p>
 		</div>
-		<h2 class="text-lg font-semibold my-5">Generator</h2>
-		<p>
-			Using object list fields with attributes, you can generate automatically a dynamic form with
-			custom rules.
-		</p>
+		<div class="bg-blue-400 shadow-lg shadow-blue-400/50 text-center p-10 rounded-xl">
+			<div class="w-full">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="16"
+					height="16"
+					fill="currentColor"
+					class="bi bi-brush w-full h-10"
+					viewBox="0 0 16 16"
+				>
+					<path
+						d="M15.825.12a.5.5 0 0 1 .132.584c-1.53 3.43-4.743 8.17-7.095 10.64a6.067 6.067 0 0 1-2.373 1.534c-.018.227-.06.538-.16.868-.201.659-.667 1.479-1.708 1.74a8.118 8.118 0 0 1-3.078.132 3.659 3.659 0 0 1-.562-.135 1.382 1.382 0 0 1-.466-.247.714.714 0 0 1-.204-.288.622.622 0 0 1 .004-.443c.095-.245.316-.38.461-.452.394-.197.625-.453.867-.826.095-.144.184-.297.287-.472l.117-.198c.151-.255.326-.54.546-.848.528-.739 1.201-.925 1.746-.896.126.007.243.025.348.048.062-.172.142-.38.238-.608.261-.619.658-1.419 1.187-2.069 2.176-2.67 6.18-6.206 9.117-8.104a.5.5 0 0 1 .596.04zM4.705 11.912a1.23 1.23 0 0 0-.419-.1c-.246-.013-.573.05-.879.479-.197.275-.355.532-.5.777l-.105.177c-.106.181-.213.362-.32.528a3.39 3.39 0 0 1-.76.861c.69.112 1.736.111 2.657-.12.559-.139.843-.569.993-1.06a3.122 3.122 0 0 0 .126-.75l-.793-.792zm1.44.026c.12-.04.277-.1.458-.183a5.068 5.068 0 0 0 1.535-1.1c1.9-1.996 4.412-5.57 6.052-8.631-2.59 1.927-5.566 4.66-7.302 6.792-.442.543-.795 1.243-1.042 1.826-.121.288-.214.54-.275.72v.001l.575.575zm-4.973 3.04.007-.005a.031.031 0 0 1-.007.004zm3.582-3.043.002.001h-.002z"
+					/>
+				</svg>
+			</div>
+			<h2 class="font-semibold my-5">Generator</h2>
+			<p class="mt-2 font-display text-md text-slate-50">
+				Using object list fields with attributes, you can generate automatically a dynamic form with
+				custom rules.
+			</p>
+		</div>
+		<div class="bg-zinc-400 shadow-lg shadow-zinc-400/50 text-center p-10 rounded-xl">
+			<div class="w-full">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="16"
+					height="16"
+					fill="currentColor"
+					class="bi bi-speedometer2 w-full h-10"
+					viewBox="0 0 16 16"
+				>
+					<path
+						d="M8 4a.5.5 0 0 1 .5.5V6a.5.5 0 0 1-1 0V4.5A.5.5 0 0 1 8 4zM3.732 5.732a.5.5 0 0 1 .707 0l.915.914a.5.5 0 1 1-.708.708l-.914-.915a.5.5 0 0 1 0-.707zM2 10a.5.5 0 0 1 .5-.5h1.586a.5.5 0 0 1 0 1H2.5A.5.5 0 0 1 2 10zm9.5 0a.5.5 0 0 1 .5-.5h1.5a.5.5 0 0 1 0 1H12a.5.5 0 0 1-.5-.5zm.754-4.246a.389.389 0 0 0-.527-.02L7.547 9.31a.91.91 0 1 0 1.302 1.258l3.434-4.297a.389.389 0 0 0-.029-.518z"
+					/>
+					<path
+						fill-rule="evenodd"
+						d="M0 10a8 8 0 1 1 15.547 2.661c-.442 1.253-1.845 1.602-2.932 1.25C11.309 13.488 9.475 13 8 13c-1.474 0-3.31.488-4.615.911-1.087.352-2.49.003-2.932-1.25A7.988 7.988 0 0 1 0 10zm8-7a7 7 0 0 0-6.603 9.329c.203.575.923.876 1.68.63C4.397 12.533 6.358 12 8 12s3.604.532 4.923.96c.757.245 1.477-.056 1.68-.631A7 7 0 0 0 8 3z"
+					/>
+				</svg>
+			</div>
+			<h2 class="font-semibold my-5">Generator</h2>
+			<p class="mt-2 font-display text-md text-slate-50">
+				Using object list fields with attributes, you can generate automatically a dynamic form with
+				custom rules.
+			</p>
+		</div>
 	</div>
-</div>
+</main>
